962a8cf142a1fc2a8cb5946f3dc449d635dda3c5
grangegermi <dasha.voloshina@gmail.com>
2022-11-24 23:42:00 +0300

    Third

--- WeatherApp/ViewController.swift
+++ WeatherApp/ViewController.swift
@@ -7,62 +7,169 @@
 
 import UIKit
 import CoreLocation
+import SnapKit
+
 
 class ViewController: UIViewController, CLLocationManagerDelegate {
     
+    var finalData = WheatherData()
+    
     let locationManager = CLLocationManager()
+    let labelMaxTemp = UILabel()
+    let labelMinTemp = UILabel()
+    let labelCity = UILabel()
+    let labelWindSpedMax = UILabel()
+    let labelTemperature = UILabel()
+    let windSpeed = UILabel()
+ 
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        view.addSubview(labelMaxTemp)
+        view.addSubview(labelMinTemp)
+        view.addSubview(labelCity)
+        view.addSubview(labelWindSpedMax)
+        view.addSubview(labelTemperature)
+        view.addSubview(windSpeed)
+        
         locationManager.delegate = self
         createLocation()
+        createConsteaints()
+        createStyle()
+        
+        let blur = UIBlurEffect(style: .systemUltraThinMaterial)
+
+        let visualEffect = UIVisualEffectView(effect: blur)
+        
+        let backround = UIImageView(frame: UIScreen.main.bounds)
+        
+        backround.image = UIImage(named: "winter")
+        backround.contentMode = UIView.ContentMode.scaleToFill
+        
+        backround.addSubview(visualEffect)
+        visualEffect.snp.makeConstraints { make in
+            make.left.equalToSuperview()
+            make.top.equalToSuperview()
+            make.bottom.equalToSuperview()
+            make.right.equalToSuperview()
+        }
+        self.view.insertSubview(backround, at: 0)
+       
+    }
+    
+    func createStyle(){
+ 
+        labelMinTemp.font = UIFont(name: "Noto Sans Oriya Bold", size: 14)
+
+        labelMaxTemp.font = UIFont(name: "Noto Sans Oriya Bold", size: 14)
+        
+        labelTemperature.font = UIFont(name: "Noto Sans Oriya Bold", size: 30)
+        labelTemperature.textAlignment = .center
+        
+        windSpeed.font = UIFont(name: "Noto Sans Oriya Bold", size: 22)
+        windSpeed.textAlignment = .center
+        
+        labelCity.font = UIFont(name: "Noto Sans Oriya Bold", size: 26)
+        labelCity.textAlignment = .center
+ 
+        labelWindSpedMax.font = UIFont(name: "Noto Sans Oriya Bold", size: 14)
+
+    }
+    
+    func createConsteaints(){
+        
+        labelCity.snp.makeConstraints { make in
+            make.left.equalToSuperview().inset(50)
+             make.top.equalToSuperview().inset(200)
+             make.width.equalTo(300)
+             make.height.equalTo(30)
+            
+         }
+        
+        labelTemperature.snp.makeConstraints { make in
+            make.left.equalToSuperview().inset(50)
+            make.top.equalToSuperview().inset(300)
+            make.width.equalTo(300)
+            make.height.equalTo(40)
+        }
+        
+        windSpeed.snp.makeConstraints { make in
+            make.top.equalToSuperview().inset(450)
+            make.left.equalTo(view.snp.left).inset(30)
+            make.width.equalTo(300)
+            make.height.equalTo(40)
+        }
+        
+        labelMinTemp.snp.makeConstraints { make in
+             make.left.equalToSuperview().inset(30)
+             make.top.equalToSuperview().inset(650)
+             make.width.equalTo(300)
+             make.height.equalTo(30)
+         }
+        
+        labelMaxTemp.snp.makeConstraints { make in
+            make.left.equalToSuperview().inset(30)
+            make.top.equalToSuperview().inset(700)
+            make.width.equalTo(300)
+            make.height.equalTo(30)
+        }
+
+        labelWindSpedMax.snp.makeConstraints { make in
+             make.left.equalToSuperview().inset(30)
+             make.top.equalToSuperview().inset(750)
+             make.width.equalTo(350)
+             make.height.equalTo(30)
+         }
+        
     }
     
     func createLocation() {
         
         locationManager.requestWhenInUseAuthorization()
-        DispatchQueue.global().async {
-            if CLLocationManager.locationServicesEnabled(){
-                if CLLocationManager.authorizationStatus() == .authorized {
-                self.locationManager.startUpdatingLocation()
-                       } else {
-                       }
-                self.locationManager.requestWhenInUseAuthorization()
-                self.locationManager.requestAlwaysAuthorization()
-                   }
-        }
-       
-    }
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        self.locationManager.startUpdatingLocation()
+
+}
+     func dataUpload() {
+         labelMaxTemp.text = "Максимальная температура: \(finalData.daily.temperature_2m_max[0]) \(finalData.daily_units.temperature_2m_max)"
+         labelMinTemp.text = "Минимальная температура: \(finalData.daily.temperature_2m_min[0]) \(finalData.daily_units.temperature_2m_max) "
+         labelWindSpedMax.text = "Максимальная скорость ветра: \(finalData.daily.windspeed_10m_max[0]) \(finalData.daily_units.windspeed_10m_max)"
+         labelCity.text = "\(finalData.timezone)"
+         labelTemperature.text = "\(finalData.current_weather.temperature) \(finalData.daily_units.temperature_2m_max) "
+         windSpeed.text = "Cкорость ветра: \(finalData.current_weather.windspeed) \(finalData.daily_units.windspeed_10m_max)"
+}
     
     func createUrl(latitude:Double,longitude:Double){
-        let url = URL(string: "https://api.open-meteo.com/v1/forecast?latitude=\(latitude.description)&longitude=\(longitude.description)&daily=temperature_2m_max,temperature_2m_min,windspeed_10m_max&timezone=Europe%2FMoscow&start_date=2022-11-24&end_date=2022-11-24")!
+        let url = URL(string: "https://api.open-meteo.com/v1/forecast?latitude=\(latitude.description)&longitude=\(longitude.description)&daily=weathercode,temperature_2m_max,temperature_2m_min,windspeed_10m_max&current_weather=true&timezone=auto&start_date=2022-11-24&end_date=2022-11-24")!
+        
         var urlRequest = URLRequest(url: url)
         urlRequest.httpMethod = "GET"
-        //        urlRequest.allHTTPHeaderFields = ["auToken":"nil"]
+        
         let task = URLSession.shared.dataTask(with: urlRequest) { data, response,error in
             guard let data = data else {return}
-            print(String(data:data, encoding: .utf8))
             
             do {
                 let jsonDecoder = JSONDecoder()
-                let finalData = try jsonDecoder.decode(WeatherData.self, from: data)
-                print(finalData.longitude.description)
+                self.finalData = try jsonDecoder.decode(WheatherData.self, from: data)
                 
+                DispatchQueue.main.async { [weak self] in
+                    self?.dataUpload()
+                }
+  
             } catch let error {
                 print(error)
             }
             
         }
         task.resume()
-        print(task)
+
     }
-    
-    
+
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
         
         if let location = locations.first {
             createUrl(latitude:location.coordinate.latitude, longitude:location.coordinate.longitude)
-            print(location.coordinate.latitude, location.coordinate.longitude)
         }
     }
 }
--- WeatherApp/WeatherData.swift
+++ WeatherApp/WeatherData.swift
@@ -4,51 +4,62 @@
 //
 //  Created by Даша Волошина on 24.11.22.
 //
+//{"latitude":53.875,
+//    "longitude":27.5625,
+//    "generationtime_ms":1.4480352401733398,
+//    "utc_offset_seconds":10800,
+//    "timezone":"Europe/Minsk"
+//    ,"timezone_abbreviation":"+03",
+//    "elevation":203.0,
+//    "current_weather":
+//        {"temperature":-3.0,
+//            "windspeed":8.3,
+//            "winddirection":56.0,
+//            "weathercode":71,
+//            "time":"2022-11-24T22:00"},
+//    "daily_units":
+//    {"time":"iso8601",
+//        "weathercode":"wmo code",
+//        "temperature_2m_max":"°C",
+//        "temperature_2m_min":"°C",
+//        "windspeed_10m_max":"km/h"},
+//    "daily":
+//    {"time":["2022-11-24"],
+//        "weathercode":[71],
+//        "temperature_2m_max":[-2.9],
+//        "temperature_2m_min":[-4.0],
+//        "windspeed_10m_max":[13.7]}}
 
 import Foundation
-
-struct WeatherData: Decodable {
-    let latitude, longitude, generationtimeMS: Double
-    let utcOffsetSeconds: Int
-    let timezone, timezoneAbbreviation: String
-    let elevation: Int
-    let dailyUnits: DailyUnits
-    let daily: Daily
-
-    enum CodingKeys: String, CodingKey {
-        case latitude, longitude
-        case generationtimeMS = "generationtime_ms"
-        case utcOffsetSeconds = "utc_offset_seconds"
-        case timezone
-        case timezoneAbbreviation = "timezone_abbreviation"
-        case elevation
-        case dailyUnits = "daily_units"
-        case daily
-    }
+ 
+struct WheatherData: Decodable {
+    
+    var longitude:Double = 0.0
+    var latitude:Double = 0.0
+    var daily = Daily()
+    var timezone:String = ""
+    var current_weather = CurrentWeather()
+    var daily_units = Units()
+    
+    init(){}
 }
 
-// MARK: - Daily
-struct Daily: Decodable {
-    let time: [String]
-    let temperature2MMax: [Double]
-    let temperature2MMin, windspeed10MMax: [Int]
-
-    enum CodingKeys: String, CodingKey {
-        case time
-        case temperature2MMax = "temperature_2m_max"
-        case temperature2MMin = "temperature_2m_min"
-        case windspeed10MMax = "windspeed_10m_max"
-    }
+struct Units:Decodable {
+    
+    var temperature_2m_max:String = ""
+    
+    var windspeed_10m_max:String = ""
 }
 
-// MARK: - DailyUnits
-struct DailyUnits: Decodable {
-    let time, temperature2MMax, temperature2MMin, windspeed10MMax: String
+struct CurrentWeather:Decodable {
+    
+    var temperature: Double = 0.0
+    var windspeed: Double = 0.0
+}
 
-    enum CodingKeys: String, CodingKey {
-        case time
-        case temperature2MMax = "temperature_2m_max"
-        case temperature2MMin = "temperature_2m_min"
-        case windspeed10MMax = "windspeed_10m_max"
-    }
+struct Daily:Decodable {
+    
+    var temperature_2m_max:[Double] = [0]
+    var temperature_2m_min:[Double] = [0]
+    var windspeed_10m_max:[Double] = [0]
 }
